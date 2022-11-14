78b11ae942b0a66fc86952f85aa0710b0b3d4a99
AnastasiaKratschmer <111846502+AnastasiaKratschmer@users.noreply.github.com>
2022-11-14 13:45:28 +0100

    updated to work with both 3 and 7 state models

--- fun_with_markovWORKS.py
+++ fun_with_markovWORKS.py
@@ -4,23 +4,35 @@ ann="CCCNNNNNNRRRRRRRRRR"
 #data="AACTGATATATATATGCC"
 
 
-def find_state(ann):
-    states=[]
-    for i in range(0,len(ann)):
-        if ann[i]=="C" and ann[i-1]!="C":
-            states.append(0)
-        if ann[i]=="C" and ann[i-1]=="C":
-            num=(states[i-1]+1)%3
-            states.append(num)
-        if ann[i]=="N":
-            states.append(3)
-        if ann[i]=="R" and ann[i-1]!="R":
-            states.append(4)
-        if ann[i]=="R" and ann[i-1]=="R":
-            num=((states[i-1]+3)%3)+4
-            states.append(num)
-    return states
+def find_state(ann, k):
+    states_3=[]
+    states_7=[]
+    if k==3:
+        for i in range(0,len(ann)):
+            if ann[i]=="C":
+                states_3.append(0)
+            if ann[i]=="N":
+                states_3.append(1)
+            if ann[i]=="R":
+                states_3.append(2)
+        return states_3
+    if k==7:
+        for i in range(0,len(ann)):
+            if ann[i]=="C" and ann[i-1]!="C":
+                states_7.append(0)
+            if ann[i]=="C" and ann[i-1]=="C":
+                num=(states_7[i-1]+1)%3
+                states_7.append(num)
+            if ann[i]=="N":
+                states_7.append(3)
+            if ann[i]=="R" and ann[i-1]!="R":
+                states_7.append(4)
+            if ann[i]=="R" and ann[i-1]=="R":
+                num=((states_7[i-1]+3)%3)+4
+                states_7.append(num)
+        return states_7
 
 
-find_state(ann)
+find_state(ann,3)
+find_state(ann,7)
 
